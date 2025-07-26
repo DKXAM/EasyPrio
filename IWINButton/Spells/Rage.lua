4d2645a943a7139ae38b8e2ed5eb558cf6cf4af7
DKXA <davidkohnstamm@gmail.com>
2025-07-26 22:07:17 +0200

    Update Rage.lua

--- IWINButton/Spells/Rage.lua
+++ IWINButton/Spells/Rage.lua
@@ -1,27 +1,6 @@
 
 IWBRage = IWBSpellBase:New("Rage")
 
-function IWBRage:ShowConfig(spell, onChange)
-    local lastFrame = IWBSpellBase.ShowConfig(self, spell, onChange)
-
-    -- Only create UI for settings relevant to Rage (min_rage, max_rage)
-    local spellType = GetSpellType(spell)
-    local schema = SPELL_TYPE_SCHEMAS[spellType]
-    if schema then
-        for settingName, settingSchema in pairs(schema) do
-            if settingName == "min_rage" or settingName == "max_rage" then
-                local settingFrame = CreateSettingControl(self.frame, settingName, settingSchema, spell, onChange)
-                if lastFrame and lastFrame.SetPoint then
-                    settingFrame:SetPoint("TOPLEFT", lastFrame, "BOTTOMLEFT", 0, 0)
-                else
-                    settingFrame:SetPoint("TOPLEFT", self.frame, "TOPLEFT", 0, 0)
-                end
-                lastFrame = settingFrame
-            end
-        end
-    end
-    return lastFrame
-end
 
 function IWBRage:IsReady(spell)
     local isReady, slot = IWBSpellBase.IsReady(self, spell)
